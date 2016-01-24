3c4e8fb1f3c2a7e14922a3ff6fc349462a575cfd
Rich Tasker <ritasker8t2@gmail.com>
2016-01-24 23:20:30 +0000

    Adds new classes to implement a file tail app.

--- Unit-1/Program.cs
+++ Unit-1/Program.cs
@@ -16,8 +16,12 @@
             var consoleWriterActorProps = Props.Create(() => new ConsoleWriterActor());
             var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterActorProps, "consoleWriterActor");
 
-            var validationActorProps = Props.Create<ValidationActor>(consoleWriterActor);
-            var validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+            Props tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            IActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
+
+            // pass tailCoordinatorActor to fileValidatorActorProps (just adding one extra arg)
+            Props fileValidatorActorProps = Props.Create(() => new FileValidatorActor(consoleWriterActor, tailCoordinatorActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(fileValidatorActorProps, "validationActor");
 
             var consoleReaderActorProps = Props.Create<ConsoleReaderActor>(validationActor);
             var consoleReaderActor = MyActorSystem.ActorOf(consoleReaderActorProps, "consoleReaderActor");
