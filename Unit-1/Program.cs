5f9b747d06575ee5514dc003777d554aaaa3b124
Rich Tasker <ritasker8t2@gmail.com>
2016-01-24 21:30:24 +0000

    Removes the validation code from the ConsoleReaderActor. Updates the Main method.

--- Unit-1/Program.cs
+++ Unit-1/Program.cs
@@ -13,8 +13,14 @@
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
-            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
+            var consoleWriterActorProps = Props.Create(() => new ConsoleWriterActor());
+            var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterActorProps, "consoleWriterActor");
+
+            var validationActorProps = Props.Create<ValidationActor>(consoleWriterActor);
+            var validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+
+            var consoleReaderActorProps = Props.Create<ConsoleReaderActor>(validationActor);
+            var consoleReaderActor = MyActorSystem.ActorOf(consoleReaderActorProps, "consoleReaderActor");
 
             // tell console reader to begin
             consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
